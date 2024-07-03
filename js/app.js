33e523466ee680ae8bc4a91166b96d309a8f8d70
LaraPsc123 <larapascarella@gmail.com>
2024-07-03 19:40:56 +0200

    ADD: if and else if

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ console.log(priceKm)
 
 
 // recupero il button
-const buttonElement = document.getElementById('calcola')
+const buttonElement = document.getElementById('calcola') // string
 console.log(buttonElement)
 
 // recupero il form 
-let formElement = document.getElementById('form-prezzo')
+let formElement = document.getElementById('form-prezzo') // string
 console.log(formElement)
 formElement.addEventListener('submit', function (event) {
     // impedire l'invio del form
@@ -24,5 +24,23 @@ formElement.addEventListener('submit', function (event) {
 
 
 // calcolo prezzo per km
-const finalKm = km * priceKm
+const finalKm = km * priceKm // number
 console.log(finalKm)
+
+// sconto del 20%
+let discount = finalKm * 0.2 // number
+let discPrice = finalKm - discount // number
+
+// SE è minorenne
+if (finalKm < 18) {
+    console.log(discPrice)
+}
+
+// sconto del 40%
+let discount40 = finalKm * 0.4 // number
+let disc40 = finalKm - discount40
+// ALTRIMENTI SE è OVER 65
+if (finalKm > 65) {
+    console.log(disc40)
+}
+
