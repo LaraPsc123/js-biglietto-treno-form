ab64cd2f4328df69ed2d01376d34fb78cb9aa1e0
LaraPsc123 <larapascarella@gmail.com>
2024-07-04 17:28:00 +0200

    ADD: all into the function

--- js/app.js
+++ js/app.js
@@ -1,54 +1,56 @@
 console.log('Calcola il prezzo')
 
-let km = inputkmElement.value // number
-let age = inputetàElement.value // number
+// recupero l'input km dal form
+const inputKmElement = document.getElementById('km')
+// recupero l'età dal form
+const inputEtàElement = document.getElementById('età')
+
 const priceKm = 0.21 // number
-console.log(km)
-console.log(age)
-console.log("prezzo al km", priceKm)
+//console.log(inputKmElement)
+//console.log(inputEtàElement)
+//console.log("prezzo al km", priceKm)
 
 
 // recupero il button
 const buttonElement = document.getElementById('calcola') // string
-console.log(buttonElement)
+//console.log(buttonElement)
 
-// recupero l'input km dal form
-const inputkmElement = document.getElementById('km')
-// recupero l'età dal form
-const inputetàElement = document.getElementById('età')
-console.log(inputkmElement)
-console.log(inputetàElement)
 
 // recupero il form 
 let formElement = document.getElementById('form-prezzo') // string
 console.log("form dal documento", formElement)
 formElement.addEventListener('submit', function (event) {
+    // calcolo prezzo per km
+    let finalKm = km * priceKm // number
+    //console.log("prezzo finale", finalKm)
+
+    // sconto del 20%
+    let discount = finalKm * 0.2 // number
+    let discPrice = finalKm - discount // number
+
+    // sconto del 40%
+    let discount1 = finalKm * 0.4
+    let disc40 = finalKm - discount1
+
+    // input dal form con value
+    const km = parseFloat(inputKmElement.value) // number
+    const age = parseFloat(inputEtàElement.value) // number
     // impedire l'invio del form
     event.preventDefault()
-    console.log("invio del submit", 'submit', event)
+    //console.log("invio del submit", 'submit', event)
+
+    // SE è minorenne
+    if (age < 18) {
+        console.log("se è minorenne", discPrice)
+    }// ALTRIMENTI SE è over 65
+    else if (age > 65) {
+        //console.log("se è over 65", disc40)
+    }
 })
 
 
 
-// calcolo prezzo per km
-let finalKm = km * priceKm // number
-console.log("prezzo finale", finalKm)
 
-// sconto del 20%
-let discount = finalKm * 0.2 // number
-let discPrice = finalKm - discount // number
-
-// SE è minorenne
-if (age < 18) {
-    console.log("se è minorenne", discPrice)
-}// ALTRIMENTI SE è over 65
-else if (age > 65) {
-    console.log("se è over 65", disc40)
-}
-
-// sconto del 40%
-let discount1 = finalKm * 0.4
-let disc40 = finalKm - discount1
 
 
 
